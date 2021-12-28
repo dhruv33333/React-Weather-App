afb8b3fc2861ae5b0c4d6d1b299f550723f0600c
dhruv33333 <dhruvnaveen.440@gmail.com>
2021-12-28 23:38:22 +0530

    Weather-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import react, { useState, useEffect } from 'react';
+
+
+
 
 function App() {
+  const [search, setSearch] = useState('denver');
+  const [data, setData] = useState([]);
+  const [input, setInput] = useState('');
+  let componentMounted = true;
+
+  useEffect(() => {
+    const fetch = async () => {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${search}&appid=76d6e5af79a3625c2afe90aab1cc5be`)
+      if (componentMounted) {
+        setData(await response.json());
+      }
+      return () => {
+        componentMounted = false;
+      }
+    }
+    fetch();
+  }, [search]);
+
+  const tempinC = (data.main.temp - 273.15).toFixed(2);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearch(input);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="card">
+        <div className="search">
+          <form onSubmit={handleSubmit}>
+            <input type="text" className="search-bar" placeholder="Search" name="search" value={input}
+              onChange={(e) => setInput(e.target.value)}
+              required />
+            <button type="submit"><svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" height="1.5em"
+              width="1.5em" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0 0 11.6 0l43.6-43.5a8.2 8.2 0 0 0 0-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z">
+              </path>
+            </svg></button>
+          </form>
+        </div>
+        <div className="weather loading">
+          <h2 className="city">Weather - {data.name}</h2>
+          <h1 className="temp">{tempinC} °C</h1>
+          <div className="flex">
+            <img src="https://openweathermap.org/img/wn/04n.png" alt="" className="icon" />
+            <div className="description">{data.weather[0].main}</div>
+          </div>
+          <div className="humidity">Humidity: {data.main.humidity} %</div>
+          <div className="wind">Wind speed: {data.wind.speed} km/h</div>
+        </div>
+      </div>
     </div>
   );
 }
